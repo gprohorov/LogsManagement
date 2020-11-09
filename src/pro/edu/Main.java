6c9e6202c4715529568e23f918c171487c83efb8
gprohorov <yuramail222@gmail.com>
2020-11-09 09:29:45 +0200

    Added File.write

--- src/pro/edu/Main.java
+++ src/pro/edu/Main.java
@@ -1,5 +1,6 @@
 package pro.edu;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,14 +8,19 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        String fileName = "/home/george/Desktop/logs.txt";
 
         LocalDateTime start = LocalDateTime.now();
+       // System.out.println(start);
+        //LocalDateTime ttt = LocalDateTime.parse("2020-11-08T22:01:46.042");
+       // System.out.println(ttt.plusDays(1));
 
         String myString = new String(Files.readAllBytes(Paths
                 .get("/home/george/Desktop/logs.txt")));
@@ -59,13 +65,35 @@ public class Main {
             logs3 += line + System.lineSeparator();
         }
 
-        Path path = Paths.get("/home/george/Desktop/logs4.txt");
+        Path path = Paths.get("/home/george/Desktop/logs5.txt");
 
         Files.write(path, logs3.getBytes());
 
+        System.out.println("-------------   stream ------------------");
+        start = LocalDateTime.now();
+
+        System.out.println(Files.lines(Paths.get(fileName))
+                .filter(line -> line.contains("ERROR"))
+                .count());
+
+
+        System.out.println(ChronoUnit.MILLIS.between(start, LocalDateTime.now()) + " ms");
+
+
+        System.out.println("-------------   scanner ------------------");
+        start = LocalDateTime.now();
 
 
 
 
+        Scanner scanner = new Scanner(new File(fileName));
+        errorLinesCount = 0;
+        while (scanner.hasNext()){
+            if(scanner.nextLine().contains("ERROR")){
+                errorLinesCount++;
+            }
+        }
+        System.out.println(errorLinesCount);
+        System.out.println(ChronoUnit.MILLIS.between(start, LocalDateTime.now()) + " ms");
     }
 }
